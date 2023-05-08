807476de32c9fbf840e579b6fa4c7c860fc1cc41
Adam <122225549+LeeAdam94@users.noreply.github.com>
2023-05-08 09:18:06 +0100

    Add files via upload

---
+++ Zombie check.py
@@ -0,0 +1,13 @@
+infected = 100
+high_risk_symptoms = ["bloodshot", "drooling", "crazed"]
+temp = int(input("What is your temperature?: "))
+
+if temp > infected:
+    symptoms = input("Do you have any symptoms?: ")
+    symptoms_list = symptoms.split(",")
+    if any(symptom in high_risk_symptoms for symptom in symptoms_list):
+        print("Put em down boys!")
+    else:
+        print("Infected, we can't let you in here. I'm very sorry")
+else:
+    print("Welcome to the safe zone!")
